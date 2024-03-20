ff7615944d8f9b13fb06950e3f4898a7dc6d7d02
Ales717 <prosenjak.ales77@gmail.com>
2024-03-20 09:40:26 +0100

    MVC

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const cors = require('cors')
+const corsOptions = require('./config/corsOptions')
 const { logger } = require('./middleware/logEvents')
 const errorHandler = require('./middleware/errorHandler')
 
@@ -10,17 +11,6 @@ const PORT = process.env.PORT || 3000;
 //custom middleware
 app.use(logger)
 
-const whitelist = ['https://www.yoursite.com/', 'http://127.0.0.1:5500', 'http://localhost:3000']
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
-    optionsSuccessStatus: 200
-}
 app.use(cors(corsOptions))
 
 //built-in middleware
@@ -29,11 +19,9 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname, '/public')))
-app.use('/subdir', express.static(path.join(__dirname, '/public')))
 
 //routes
 app.use('/', require('./routes/root'))
-app.use('/subdir', require('./routes/subdir'))
 app.use('/employees', require('./routes/api/employees'))
 
 
