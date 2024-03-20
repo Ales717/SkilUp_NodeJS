9373a544e0fec5ff355964620d49fae98eaf621b
Ales717 <prosenjak.ales77@gmail.com>
2024-03-20 08:34:25 +0100

    routing

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const cors = require('cors')
-const { logger }= require('./middleware/logEvents')
+const { logger } = require('./middleware/logEvents')
 const errorHandler = require('./middleware/errorHandler')
 
 const PORT = process.env.PORT || 3000;
@@ -12,10 +12,10 @@ app.use(logger)
 
 const whitelist = ['https://www.yoursite.com/', 'http://127.0.0.1:5500', 'http://localhost:3000']
 const corsOptions = {
-    origin: (origin, callback) =>{
+    origin: (origin, callback) => {
         if (whitelist.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
-        } else{
+        } else {
             callback(new Error('Not allowed by CORS'))
         }
     },
@@ -25,33 +25,25 @@ app.use(cors(corsOptions))
 
 //built-in middleware
 app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-app.use(express.static(path.join(__dirname, '/public')))
 
-app.get('^/$|index(.html)?', (req, res) => {
-    //res.sendFile('./views/index.html', { root: __dirname})
-    res.sendFile(path.join(__dirname, 'views', 'index.html'))
-})
-app.get('/new-page.(html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
-})
-app.get('/old-page(.html)?', (req, res) => {
-    res.redirect(301, '/new-page.html')
-})
-app.get('/hello(.html)?', (req, res, next) => {
-    console.log("attempted to load hello.html")
-    next()
-}, (req, res) => {
-    res.send('Hello world!')
-})
+app.use(express.json())
+
+app.use(express.static(path.join(__dirname, '/public')))
+app.use('/subdir', express.static(path.join(__dirname, '/public')))
+
+//routes
+app.use('/', require('./routes/root'))
+app.use('/subdir', require('./routes/subdir'))
+app.use('/employees', require('./routes/api/employees'))
+
 
 app.all('/*', (req, res) => {
     res.status(404)
     if (req.accepts('html')) {
         res.sendFile(path.join(__dirname, 'views', '404.html'))
-    }else if (req.accepts('json')) {
-        res.json({ error: "404 not Found"})
-    }else{
+    } else if (req.accepts('json')) {
+        res.json({ error: "404 not Found" })
+    } else {
         res.type('txt').send("404 not Found")
     }
 })
